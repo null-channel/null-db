1b3edb12d8a8efc1d7e34e6436af4d05edba63d5
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-01-19 08:27:30 +0100

    go fmt

--- go/all-in-memory/pkg/kv/kv.go
+++ go/all-in-memory/pkg/kv/kv.go
@@ -3,37 +3,34 @@ package kv
 import "sync"
 
 type KVStore struct {
-     mu sync.RWMutex
-     store map[string]string
+	mu    sync.RWMutex
+	store map[string]string
 }
 
-func NewKv() *KVStore{
+func NewKv() *KVStore {
 	return &KVStore{
 		store: map[string]string{},
 	}
 }
 
-// Get returns the value of k  
-func ( kv *KVStore) Get(k string) string{
+// Get returns the value of k
+func (kv *KVStore) Get(k string) string {
 	defer kv.mu.RUnlock()
 	kv.mu.RLock()
 	val := kv.store[k]
-	return val 
+	return val
 }
 
-func (kv *KVStore) Set(k,v string) string {
+func (kv *KVStore) Set(k, v string) string {
 	defer kv.mu.RUnlock()
 	kv.mu.RLock()
-	kv.store[k] =  v 
-	return k 
+	kv.store[k] = v
+	return k
 }
 
-func (kv *KVStore) Delete(k string) string{
-   defer kv.mu.RUnlock()
-   kv.mu.Lock()
-   delete(kv.store,k)
-   return "true"
+func (kv *KVStore) Delete(k string) string {
+	defer kv.mu.RUnlock()
+	kv.mu.Lock()
+	delete(kv.store, k)
+	return "true"
 }
-
-
-

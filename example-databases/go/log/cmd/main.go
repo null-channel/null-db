b6255e1c00369e75f76f2c2df06489d8f7ea4ece
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-03-31 14:58:34 +0100

    proof of concept set and get

--- example-databases/go/log/cmd/main.go
+++ example-databases/go/log/cmd/main.go
@@ -1,7 +1,47 @@
 package main
 
-// var (
-// 	id, _    = shortid.Generate()
-// 	serverid = fmt.Sprintf("[logdb-%s] ", id)
-// 	l        = log.New(os.Stdout, serverid, log.LstdFlags)
-// )
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/null-channel/null-db/pkg/db"
+	"github.com/teris-io/shortid"
+)
+
+var (
+	createlogfile sync.Once
+	logfile       = "log.db"
+	id, _         = shortid.Generate()
+
+	// unique server id incase this ever becomes distributed??
+	serverid = fmt.Sprintf("[logdb-%s] ", id)
+	l        = log.New(os.Stdout, serverid, log.LstdFlags)
+)
+
+func main() {
+
+	// create log file once server starts up
+	createlogfile.Do(func() {
+		_, err := os.Create(logfile)
+		if err != nil {
+			fmt.Println(err)
+		}
+	})
+	DB := db.NewDB(l, logfile)
+	err := DB.Set("hello", "world")
+	if err != nil {
+		l.Println(err)
+	}
+
+	err = DB.Set("hello", "man")
+	if err != nil {
+		l.Println(err)
+	}
+	str, err := DB.Get("hello")
+	if err != nil {
+		l.Println(err)
+	}
+	l.Println(str)
+}

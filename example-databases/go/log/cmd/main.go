52fb1e11370efd8a43a6fdb07e9e4be9486b4a07
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-04-01 15:37:27 +0100

    add handlers

--- example-databases/go/log/cmd/main.go
+++ example-databases/go/log/cmd/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"sync"
 
+	"github.com/gorilla/mux"
 	"github.com/null-channel/null-db/pkg/db"
+	"github.com/null-channel/null-db/pkg/server"
 	"github.com/teris-io/shortid"
 )
 
@@ -30,4 +33,15 @@ func main() {
 		}
 	})
 	DB := db.NewDB(l, logfile)
+	loghandler := server.NewDbServer(l, DB)
+	router := mux.NewRouter()
+	router.HandleFunc("/get", loghandler.GetHandler).Methods(http.MethodPost)
+	router.HandleFunc("/set", loghandler.InsertHandler).Methods(http.MethodPost)
+	router.HandleFunc("/pop", loghandler.DeleteHandler).Methods(http.MethodPost)
+	l.Println("starting server on port 4000")
+	http.Handle("/", router)
+	err := http.ListenAndServe(":4000", nil)
+	if err != nil {
+		l.Fatalf("unable to start server on 4000 %s", err.Error())
+	}
 }
